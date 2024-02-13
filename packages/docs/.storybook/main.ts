49ca5f822cfd038d13756a7385d82ad24b32074d
Pedro Rodrigues <pedro.roguea@gmail.com>
2024-02-13 01:33:56 -0300

    fix: modify .storybook config stories paths

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -10,7 +10,10 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
+  stories: [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
